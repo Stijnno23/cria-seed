b24d62bec81151a748ea5a8fc27806df8bcf7751
StijnL <lous23@live.nl>
2015-06-03 11:56:53 +0200

    Refactored the modelviewer so it is in the public directory. Also added it to the menu

---
+++ public/modules/modelviewer/config/modelviewer.client.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('modelviewer').run(['Menus',
+    function(Menus) {
+        // Set top bar menu items
+        Menus.addMenuItem('topbar', 'Modelviewer', 'modelviewer', 'dropdown', '/dog(/create)?');
+        Menus.addSubMenuItem('topbar', 'modelviewer', 'modelviewerlist', 'modelviewer');
+        Menus.addSubMenuItem('topbar', 'modelviewer', 'modelviewer create', ' modelviewer/create');
+    }
+]);
---
+++ public/modules/modelviewer/config/modelviewer.client.routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Setting up modelviewer
+angular.module('modelviewer').config(['$stateProvider',
+    function ($stateProvider) {
+        // Modelviewer state routing
+        $stateProvider.
+            state('createModelviewer', {
+                url: '/modelviewer/',
+                templateUrl: 'modules/modelviewer/views/create-modelviewer.client.view.html'
+            });
+    }
+]);
