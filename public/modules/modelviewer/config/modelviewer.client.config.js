935b04c5fd4fe3154ae575cb7194c950443faf49
Dion <dion_westmaas@msn.com>
2015-06-03 12:47:41 +0200

    Translation updates

--- public/modules/modelviewer/config/modelviewer.client.config.js
+++ public/modules/modelviewer/config/modelviewer.client.config.js
@@ -4,7 +4,7 @@
 angular.module('modelviewer').run(['Menus',
     function(Menus) {
         // Set top bar menu items
-        Menus.addMenuItem('topbar', 'Modelviewer', 'modelviewer', 'dropdown', '/dog(/create)?');
+        Menus.addMenuItem('topbar', 'Modelviewer', 'modelviewer', 'dropdown', '/modelviewer(/create)?');
         Menus.addSubMenuItem('topbar', 'modelviewer', 'modelviewerlist', 'modelviewer');
         Menus.addSubMenuItem('topbar', 'modelviewer', 'modelviewer create', ' modelviewer/create');
     }
