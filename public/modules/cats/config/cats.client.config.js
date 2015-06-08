8a3712c07db0e6d5f98fc0af538c672d1be641e1
Dion <dion_westmaas@msn.com>
2015-06-08 18:36:20 +0200

    Translation fixes

--- public/modules/cats/config/cats.client.config.js
+++ public/modules/cats/config/cats.client.config.js
@@ -6,8 +6,8 @@
 angular.module('cats').run(['Menus',
     function (Menus) {
         // Set top bar menu items
-        Menus.addMenuItem('topbar', 'Cats', 'cats', 'dropdown', '/cats(/create)?');
-        Menus.addSubMenuItem('topbar', 'cats', 'List Cat', 'cats');
-        Menus.addSubMenuItem('topbar', 'cats', 'New Cat', 'cats/create');
+        Menus.addMenuItem('topbar', 'Katten', 'cats', 'dropdown', '/cats(/create)?');
+        Menus.addSubMenuItem('topbar', 'cats', 'Lijst van katten', 'cats');
+        Menus.addSubMenuItem('topbar', 'cats', 'Nieuwe kat', 'cats/create');
     }
     ]);
--- public/modules/dogs/config/dogs.client.config.js
+++ public/modules/dogs/config/dogs.client.config.js
@@ -6,8 +6,8 @@
 angular.module('dogs').run(['Menus',
     function (Menus) {
         // Set top bar menu items
-        Menus.addMenuItem('topbar', 'Dogs', 'dogs', 'dropdown', '/dog(/create)?');
-        Menus.addSubMenuItem('topbar', 'dogs', 'List Dogs', 'dogs');
-        Menus.addSubMenuItem('topbar', 'dogs', 'New Dog', 'dogs/create');
+        Menus.addMenuItem('topbar', 'Honden', 'dogs', 'dropdown', '/dog(/create)?');
+        Menus.addSubMenuItem('topbar', 'dogs', 'Lijst van honden', 'dogs');
+        Menus.addSubMenuItem('topbar', 'dogs', 'Nieuwe hond', 'dogs/create');
     }
     ]);
